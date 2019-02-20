250ec8cbdeb93caa156ef9103d34e128b020f702
RayGervais <raygervais@hotmail.ca>
2019-02-19 23:27:08 -0500

    Adds: Vue Project, TodoList, Todos, and Header Components

---
+++ Posts/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <Header />
+    <Todos 
+      v-bind:todos="todos" 
+      v-on:del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+import Header from './components/layout/header';
+
+export default {
+  name: 'app',
+  components: {
+    Todos,
+    Header
+  },
+  data () {
+    return {
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id != id);
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Posts/src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+    <div 
+        class="todo-item" 
+        v-bind:class="{'is-complete': todo.completed}">
+        
+        <input 
+            type="checkbox" 
+            v-on:change="markComplete">
+
+        {{todo.title}}
+
+        <button 
+            @click="$emit('del-todo', todo.id)"
+            class="del">X</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = true;
+        }
+    }
+}
+</script>
+
+<style>
+.todo-item {
+    padding: 8px 16px;
+    background: #f4f4f4;
+    border-bottom: 1px dotted #ccc;
+    margin: auto;
+}
+
+.is-complete {
+    text-decoration: line-through;
+}
+
+.del {
+    float: right;
+    border-radius: 24px;
+    border: solid 1px #AAA;
+    background: #FFF;
+}
+</style>
+
+
