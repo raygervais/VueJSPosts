d25f97c8e1255b590caee73fc85b5d0fad85edda
RayGervais <raygervais@hotmail.ca>
2019-03-03 15:06:51 -0500

    adds: Route view

--- Posts/src/router.js
+++ Posts/src/router.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from './views/Home.vue'
+import Root from './views/Root.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'root',
-    //   component: Root
-    // },
+    {
+      path: '/',
+      name: 'root',
+      component: Root
+    },
     {
       path: '/home',
       name: 'home',
---
+++ Posts/src/views/Root.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app" class="container">
+        <h2>Hello Friend!</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Root'
+}
+</script>
+
